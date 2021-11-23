ec0ff41e78deddb903025f43f3bdeede2cbd930a
Gina Marcela Cruz <gmcruz@unicauca.edu.co>
2021-11-23 00:27:29 -0500

    add variables prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
+  API_URL: 'https://whispering-springs-44329.herokuapp.com/',
+  URL_IMAGES: 'https://www.themoviedb.org/t/p/w440_and_h660_face/',
   production: true
 };
